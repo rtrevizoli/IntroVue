b82b5266867b22163291206f958eb410c468b023
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2020-03-22 11:24:19 -0300

    Stylesheet and conditional exibition.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        message: "Brazilian Cotton Socks"
+        product: "Socks",
+        message: "Brazilian Cotton Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        url: "#",
+        inventory: 100,
+        onSale: true
     }
 })
